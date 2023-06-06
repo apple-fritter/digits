9ae184a0cb77216e2d3e6c4370660e6ea477fada
Ａｐｐｌｅ Ｆｒｉｔｔｅｒ <125221355+apple-fritter@users.noreply.github.com>
2023-06-06 16:57:31 -0700

    Create digitalpha.sh

---
+++ digitalpha.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <file_path>"
+  exit 1
+fi
+
+input_file="$1"
+output_file="sanitized.txt"
+
+if [ ! -f "$input_file" ]; then
+  echo "File not found: $input_file"
+  exit 1
+fi
+
+# Remove non-numeric, non-punctuational, and non-alphabetical characters, and collapse repeated punctuations
+cat "$input_file" | tr -cs '[:alnum:][:punct:]' '\n' | awk '!a[$0]++' > "$output_file"
+
+echo "Sanitized file created: $output_file"
